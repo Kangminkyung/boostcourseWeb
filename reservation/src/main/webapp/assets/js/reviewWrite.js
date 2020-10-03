b4ddb15378901a6ab5241bf44fbc73e0550d945b
kmk <swkang1994@nate.com>
2020-10-03 12:55:51 +0900

    별점 추가

--- reservation/src/main/webapp/assets/js/reviewWrite.js
+++ reservation/src/main/webapp/assets/js/reviewWrite.js
@@ -10,6 +10,7 @@ function Review(){
 
 Review.prototype.init = function(){
 	Review.prototype.initTitle();
+	Review.prototype.initRatingPoint();
 };
 
 /* id 값 불러오기 */
@@ -46,7 +47,59 @@ Review.prototype.showTitle = function(res){
 };
 
 /* 별점 */
-
+Review.prototype.initRatingPoint = function(){
+	let reviewRating = document.querySelector(".review_rating");
+	let rating = 0; // rating 배열 순서 체크
+	let visited = [];
+	let ratingList = Array.prototype.slice.call(reviewRating.querySelector(".rating").childNodes).filter((element) => {
+		if(element.nodeName === "INPUT"){
+			visited.push(false);
+			return true;
+		}
+	});
+	
+	reviewRating.addEventListener("click", function(e){
+		if(e.target.tagName === "INPUT"){
+			let clickedRating = parseInt(e.target.value);
+			console.log(clickedRating);
+			
+			// 현재 별점이 클릭한 별점보다 크면 checked 추가
+			if(rating < clickedRating){
+				for(let i = rating; i < clickedRating; i++){
+					console.log(i);
+					console.log(clickedRating);
+					ratingList[i].classList.add("checked");
+					console.log(ratingList[i]);
+				}
+				// 현재 별점이 클릭한 별점보다 작으면 check 해제
+			}else if(rating > clickedRating){
+				for(let i = rating - 1; i>clickedRating -1; i--){
+					ratingList[i].classList.remove("checked");
+					console.log(i);
+					console.log(clickedRating);
+					console.log(ratingList[i]);
+					if(visited[i] === false){
+						ratingList[i].classList.remove("checked");
+					}else{
+						ratingList[i].click();
+					}
+				}
+			}
+			
+			if(visited[clickedRating -1] === false){
+				visited[clickedRating - 1] = true;
+			}else{
+				visited[clickedRating -1] = false;
+			}
+			rating = clickedRating;
+			document.querySelector(".star_rank").innerText = rating;
+			if(document.querySelector(".star_rank").innerText > 0){
+				document.querySelector(".star_rank").classList.remove("gray_star");
+			}
+		}
+	});
+	console.log(document.querySelector(".star_rank.gray_star"));
+};
 
 /* 리뷰 텍스트*/
 
